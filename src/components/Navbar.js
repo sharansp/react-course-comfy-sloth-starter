01590b4c78e042f8450bef3da73b250bf4203603
SHARAN <sharangowda2425@gmail.com>
2022-08-16 11:55:20 +0530

    wip

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,17 +9,41 @@ import { useProductsContext } from '../context/products_context'
 import { useUserContext } from '../context/user_context'
 
 const Nav = () => {
-  return <h4>navbar</h4>
+  return(
+  <NavContainer>
+    <div className='nav-center'>
+    <div className='nav-header'>
+      <Link to='/'>
+        <img src={logo} alt='logo'></img>
+      </Link>
+      <button type='button' className='nav-toggle'>
+        <FaBars/>
+      </button>
+    </div>
+    <ul className='nav-links'>
+      {links.map((link)=>{
+        const {id,text,url}=link;
+        return <li key={id}>
+          <Link to={url}>
+            {text}
+          </Link>
+        </li>
+      })}
+    </ul>
+    <CartButtons/>
+    </div>
+  </NavContainer>
+  ) 
 }
 
 const NavContainer = styled.nav`
-  height: 5rem;
+  height: 4rem;
   display: flex;
   align-items: center;
   justify-content: center;
 
   .nav-center {
-    width: 90vw;
+    width: 100vw;
     margin: 0 auto;
     max-width: var(--max-width);
   }
